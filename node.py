000d6b4d1b0ee940f61059e6662dcc3b64d0d4dc
Simon Peter <speter3@illinois.edu>
2015-04-16 23:47:14 -0500

    Added ack and commented

--- node.py
+++ node.py
@@ -10,9 +10,16 @@ import signal
 from sys import platform
 
 def add_key(key):
+    """
+    Add key to node's store
+    """
     keys.append(key)
 
 def handle_message(data):
+    """
+    Decode message and determine task
+    :param data: Message string
+    """
     message = json.load(data)
 
     if 'action' in message:
@@ -22,10 +29,13 @@ def handle_message(data):
             add_key(message['key'])
 
 def listening_thread(buffer_size):
+    """
+    Receive incoming messages
+    """
     global s
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((TCP_IP, node_port))
+    s.bind(('', node_port))
     s.listen(1)
     while 1:
         conn, addr = s.accept()
@@ -34,11 +44,40 @@ def listening_thread(buffer_size):
         conn.close()
 
 def term_handler(signal):
+    """
+    Gracefully exit when encountering SIGTERM
+    """
     s.close()
     exit(0)
 
+def send_message(port, data):
+    """
+    Create socket and send data
+    """
+    s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s2.connect(('', coordinator_port))
+    s2.send(data)
+    s2.close()
+
+def create_action(action):
+    """
+    Create an 'action' dictionary entry
+    :param action: Type of action
+    :return: Dictionary entry
+    """
+    return {'action':action}
+
+def send_ack():
+    """
+    Send acknowledgement to coordinator thread
+    """
+    encoded_string = json.dump(create_action('ack'))
+    send_message(coordinator_port, encoded_string)
+
 def main():
-    global TCP_IP
+    """
+    Initialize node variables, start listener thread, send ack to coordinator
+    """
     global coordinator_port
     global node_port
     global keys
@@ -46,14 +85,6 @@ def main():
     global succ_port
 
     signal.signal(signal.SIGTERM, term_handler)
-
-    if platform == "darwin":
-        TCP_IP = socket.gethostbyname(socket.gethostname())
-    elif platform == "linux" or platform == "linux2":
-        TCP_IP = '10.0.0.6'
-    else:
-        print "Operating system not supported"
-        exit(1)
 
     coordinator_port = sys.argv[1]
     node_port = sys.argv[2]
@@ -64,6 +95,7 @@ def main():
     listener.daemon = True
     listener.start()
 
+    send_ack()
 
 if __name__ == "__main__":
     main()
