787d2f1f538bc70fa57ede7cebd5a6ebd44e80fc
Simon Peter <speter3@illinois.edu>
2015-04-18 04:07:40 -0500

    Implemented find, not tested

--- node.py
+++ node.py
@@ -9,6 +9,51 @@ import threading
 import signal
 from sys import platform
 
+def send_found_message():
+    """
+    Send successor of current node because that will be where the key is located
+    """
+    encoded_string = json.dumps({'action':'ACK', 'found':str(succ_port)})
+    send_message(coordinator_port, encoded_string)
+
+def ask_next_node_for_successor(next_node, key_to_find):
+    """
+    Ask the next node to run find_successor
+    """
+    encoded_string = json.dumps({'action':'FIND', 'key':key_to_find})
+    send_message(next_node, encoded_string)
+
+
+def find_successor(key_to_find):
+    """
+    Finds the predecessor by moving forward in the chord circle till it finds a node whose value is less than the key \
+    successor is greater than the given key
+    :param key_to_find:
+    :return:predecessor of given key
+    """
+    current_node = coordinator_port
+    current_successor = succ_port
+
+    if key_to_find > current_node and key_to_find < current_successor: #TODO: Convert port to id numbers
+        #Predecessor found!
+        send_found_message()#TODO:Return successor
+    else:
+        next_node = closest_preceding_finger(key_to_find)
+        ask_next_node_for_successor(next_node, key_to_find)
+
+def closest_preceding_finger(key_to_find):
+    """
+    Scans the finger table from bottom to top to determine the closest known predecessor of the given key
+    :param key_to_find:
+    :return:closest known predecessor of given key
+    """
+    for i in range(m,1,-1):
+        finger_node = succesors[i]
+        if finger_node > coordinator_port and finger_node < key_to_find:
+            return finger_node
+        return coordinator_port #Don't understand how it could ever reach here? Won't the key be between one of the fingertable entries?
+
+
 def add_keys(keys_to_add):
     """
     Add key to node's store
@@ -61,37 +106,39 @@ def send_message(port, data):
     s2.send(data)
     s2.close()
 
-def create_action(action):
-    """
-    Create an 'action' dictionary entry
-    :param action: Type of action
-    :return: Dictionary entry
-    """
-    return {'action':action}
-
 def send_ack():
     """
     Send acknowledgement to coordinator thread
     """
-    encoded_string = json.dumps(create_action('ack'))
+    encoded_string = json.dumps({'action':'ACK'})
     send_message(coordinator_port, encoded_string)
 
 def main():
     """
     Initialize node variables, start listener thread, send ack to coordinator
     """
-    global coordinator_port
+    global coordinator_port #TODO: Need to remember start port for node checking logic
     global node_port
-    global keys
     global pred_port
     global succ_port
+    global m
+
+    global keys
+
+    global finger_starts
+    global intervals
+    global succesors
 
     signal.signal(signal.SIGTERM, term_handler)
 
     coordinator_port = int(sys.argv[1])
     node_port = int(sys.argv[2])
     json_data = sys.argv[3]
+    m = 5 #TODO: Currently hardcoding identifier size
     keys = []
+    finger_starts = []
+    intervals = []
+    successors = []
 
     send_ack()
     start_listening()
